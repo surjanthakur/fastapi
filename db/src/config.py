16817d25ce0d113ffe8b7e5d3ef31bfad9b6cbd2
surjanthakur <tsurjan506@gmail.com>
2026-02-18 14:26:10 +0530

    save

--- db/src/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Settings(BaseSettings):
-    db_url: str
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        case_sensitive=False,
-    )
