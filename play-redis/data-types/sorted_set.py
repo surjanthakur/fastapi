c461d752219b2fc6566371cf831d89d520423fce
surjanthakur <tsurjan506@gmail.com>
2026-02-21 10:47:45 +0530

    save

--- play-redis/data-types/sorted_set.py
+++ play-redis/data-types/sorted_set.py
@@ -8,7 +8,8 @@ def sorted_init():
     rds.zadd(
         "players", {"roy": 20, "aman": 45, "ashi": 5, "gill": 3}
     )  #!  to add values in sorted set
-    rds.zrange("players", 0, -1)  # * get values accourding to range
+    rds.zrange("players", 0, -1)  # * get members accourding to range [asc] order.
+    rds.zrevrange("players", 0 - 1)  # * get members accounrding to range reverse order.
     rds.zrem("players", "aman")  # * remove the values
     rds.zscore("players", "roy")  # * get the score of a member
     rds.zrank("players", "roy")  # * return the member index from sorted_set [0]
