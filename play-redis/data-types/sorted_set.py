6f8908520dbcd06360a907ad65f55dcc9ef4c031
surjanthakur <tsurjan506@gmail.com>
2026-02-21 10:44:47 +0530

    svae

---
+++ play-redis/data-types/sorted_set.py
@@ -0,0 +1,26 @@
+import redis
+
+
+rds = redis.Redis(port=6379, host="localhost", db=0)
+
+
+def sorted_init():
+    rds.zadd(
+        "players", {"roy": 20, "aman": 45, "ashi": 5, "gill": 3}
+    )  #!  to add values in sorted set
+    rds.zrange("players", 0, -1)  # * get values accourding to range
+    rds.zrem("players", "aman")  # * remove the values
+    rds.zscore("players", "roy")  # * get the score of a member
+    rds.zrank("players", "roy")  # * return the member index from sorted_set [0]
+    rds.zpopmax(
+        "players"
+    )  # * return and delete the maximum score member from [sorted_set].
+    # if the set last value is poped it removes the set
+    rds.zpopmin(
+        "players"
+    )  # * return and delete the lowest score member from [sorted_set].
+    # delete the sorted_set if last member poped
+    rds.zrandmember("players")  # return one or more  random member from sorted_set.
+
+
+sorted_init()
